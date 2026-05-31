1cac178a00125cba38e57748836dcda6f2458120
AJ Tech City <arunjai12456@icloud.com>
2026-05-31 23:22:51 +0100

    Week 6 Complete

--- Week 6/answers.hs
+++ Week 6/answers.hs
@@ -27,3 +27,26 @@ getAtAddress (Branch _ xs) (i:is) =
         childCount = length xs
     in
         if i < 0 || i > childCount-1 then Nothing else getAtAddress (xs!!i) is
+
+------------------------------------
+
+--Exercise 1 - Applying Functions to Trees
+data Tree a b = Leaf b | Fork (Tree a b) a (Tree a b) deriving (Eq, Show)
+
+--Implementation Task - Write higher-order func that takes two functions as well as a Tree a b element as input and applies the first function to values at the forks and second function to the values at the leaves.
+applyfuns :: (a->c) -> (b->d) -> Tree a b -> Tree c d
+applyfuns _ g (Leaf x) = Leaf (g x)
+applyfuns f g (Fork l a r ) = Fork (applyfuns f g l) (f a) (applyfuns f g r)
+
+--Exercise 2 - Updating Nodes along a route
+data BinTree a = Empty | Node (BinTree a) a (BinTree a) deriving (Eq, Show)
+
+data BinDirection = GoLeft | GoRight deriving (Eq, Show, Bounded, Enum)
+type Route = [BinDirection]
+
+--Implementation Task = Implement a function which applies a function to the values of all nodes along a given route in a tree
+updateNodes :: Route -> (a -> a) -> BinTree a -> BinTree a
+updateNodes _ _  Empty = Empty
+updateNodes [] f (Node l x r) = Node l (f x) r
+updateNodes (GoLeft : route) f (Node left x right) = Node (updateNodes route f left) (f x) right
+updateNodes (GoRight: route) f (Node left x right) = (Node left (f x) (updateNodes route f right))
